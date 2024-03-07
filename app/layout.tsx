90abb46b6471635372633755fd891957443e72b0
Ali Ozzaim <125793435+Aliozzaim@users.noreply.github.com>
2024-03-07 12:31:03 +0100

    add : google

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,16 +19,17 @@ export default function RootLayout({
       <Head>
         <script
           async
-          src="https://www.googletagmanager.com/gtag/js?id=G-FDW279YMXJ"
-        />
+          src="https://www.googletagmanager.com/gtag/js?id=G-BJQDL2NJJ8"
+        ></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag() { dataLayer.push(arguments); }
-              gtag('js', new Date());
-              gtag('config', 'G-FDW279YMXJ');
-            `,
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-BJQDL2NJJ8');
+              `,
           }}
         />
         <script

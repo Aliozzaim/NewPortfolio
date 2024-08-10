5598d9a4543d97c40572c4811b7596658d9c8c32
Ali Ozzaim <125793435+Aliozzaim@users.noreply.github.com>
2024-08-10 18:59:42 +0200

    add new blogpost

--- components/BlogCard.jsx
+++ components/BlogCard.jsx
@@ -9,17 +9,18 @@ import { HoverEffect } from "@/components/ui/Cardhovereffect"
 const items = [
   {
     id: 1,
+    title:
+      "Fixing vue-js-modal Library for Vue 3: A Guide to Restoring Modal Functionality",
+    description:
+      "I’ve put together a comprehensive guide detailing the changes I made to get vue-js-modal working seamlessly with Vue 3. If you’re dealing with similar issues, this article might be just what you need!",
+    link: "https://dev.to/aliozzaim/fixing-vue-js-modal-library-for-vue-3-a-guide-to-restoring-modal-functionality-2lai",
+  },
+  {
+    id: 2,
     title: "Creating 3D Art Gallery with Three.js",
     description:
       "Learn how to create a 3D art gallery using Three.js and React.",
     link: "https://dev.to/aliozzaim/creating-a-3d-gallery-app-with-threejs-html-and-css-part-1-2gcf",
-  },
-  {
-    id: 2,
-    title: "Building Scalable Applications",
-    description:
-      "Learn how to build scalable applications using best practices in React and Next.js.",
-    link: "https://dev.to/aliozzaim",
   },
   {
     id: 3,
